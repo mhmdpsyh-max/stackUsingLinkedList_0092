d1997198eb5df6873238e8352c3fd5bd4d60ee2c
pasyaa <mhmdpsyh@gmail.com>
2026-05-14 19:17:02 +0700

    void pop(){

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -15,3 +15,25 @@ class Node
 
 // stack class
 class stack
+{
+    private:
+    Node *top;
+
+    public:
+    stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    void pop(){
+        
+}
